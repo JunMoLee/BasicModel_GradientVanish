a6efce9fb397206f828b440c4eab80a7e66aa572
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2021-08-09 19:38:21 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -142,6 +142,14 @@ int main() {
 		printf("\tWrite latency=%.4e s\n", subArrayIH->writeLatency + subArrayHO->writeLatency);
 		printf("\tRead energy=%.4e J\n", arrayIH->readEnergy + subArrayIH->readDynamicEnergy + arrayHO->readEnergy + subArrayHO->readDynamicEnergy);
 		printf("\tWrite energy=%.4e J\n", arrayIH->writeEnergy + subArrayIH->writeDynamicEnergy + arrayHO->writeEnergy + subArrayHO->writeDynamicEnergy);
+		NL_Gp = param->NL_Gp;
+		NL_Gn = param->NL_Gn;
+		
+		fstream read;
+		char str[1024];
+		sprintf(str, "NL_%.2f_%.2f.csv" ,NL_Gp, NL_Gn );
+		read.open(str,fstream::app);     
+		read<< i<<", "<<(double)correct/param->numMnistTestImages*100;
 	}
 	printf("\n");
 	return 0;
